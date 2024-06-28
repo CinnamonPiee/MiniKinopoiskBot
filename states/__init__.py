e7fa65f68a1745d6ad39e5a818a1b9934b182710
Simon Shakhanov <vonahkahs.s@gmail.com>
2024-06-28 14:50:33 +0400

    some syntax fix

--- states/__init__.py
+++ states/__init__.py
@@ -2,3 +2,7 @@ from . import custom_searching
 from . import find_film_serial
 from . import history_of_search
 from . import registration
+from . import random_film_serial
+from . import main_menu
+from . import low_coast_film_or_serial
+from . import hight_coast_film_or_serial
