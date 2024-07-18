1124394778d1723d0d4ae895d0f4dd8d102d5aee
CinnamonPie <vonahkahs.s.ved@gmail.com>
2024-07-18 16:55:56 +0400

    create migrations again and fix connection with api

--- api/random_movie_serial_search.py
+++ api/random_movie_serial_search.py
@@ -5,20 +5,34 @@ from typing import Optional
 
 
 def random_movie_search(
-        year,
-        rating,
-        age_rating,
-        movie_length,
-        janr,
-        country
+        year: Optional[str] = None,
+        rating: Optional[str] = None,
+        age_rating: Optional[str] = None,
+        movie_length: Optional[str] = None,
+        janr=None,
+        country: Optional[list] = None
 ):
-    url = f"https://api.kinopoisk.dev/v1.4/movie/random?notNullFields=name&type=movie&"\
-          f"year={year}&"\
-          f"rating.imdb={rating}&"\
-          f"ageRating={age_rating}&"\
-          f"movieLength={movie_length}&"\
-          f"genres.name={janr}&"\
-          f"countries.name={country}"
+
+    url = f"https://api.kinopoisk.dev/v1.4/movie/random?notNullFields=name&type=movie"
+
+    if year:
+        url = url + f"&year={year}"
+    if rating:
+        url = url + f"&rating.imdb={rating}"
+    if age_rating:
+        url = url + f"&ageRating={age_rating}"
+    if movie_length:
+        url = url + f"&movieLength={movie_length}"
+    if janr:
+        if len(janr) == 1:
+            url = url + f"&genres.name={janr[0]}"
+        for i in janr:
+            url = url + f"&genres.name=+{i}"
+    if country:
+        if len(country) == 1:
+            url = url + f"&countries.name={country}"
+        for j in country:
+            url = url + f"&countries.name=+{j}"
 
     headers = {
         "accept": "application/json",
@@ -36,4 +50,4 @@ def random_movie_search(
         return "Сервер временно не доступен, попробуйте позже!"
 
 
-pprint.pprint(random_movie_search(2021, [], [], [], "null", "null"))
+pprint.pprint(random_movie_search(janr=["ужасы", "боевик"]))
